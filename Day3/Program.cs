ca0ccc7aa781c16483097f8aa3c8b4484562cc9d
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2024-12-16 21:56:38 +0100

    Day3 Part2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -10,33 +10,44 @@ class Program
         try
         {
             var inputText = await ReadingPuzzleOperations.GetInputTextAsync();
-            var pattern = @"mul\((\d{1,3},\d{1,3})\)";
+            var inputTextWithoutDonts = RemoveDonts(inputText);
             
-            var options = RegexOptions.Multiline;
-            var result = 0;
-
-            foreach (Match m in Regex.Matches(inputText, pattern, options))
-            {
-                Console.WriteLine($"Value: {m.Value} Index: {m.Index} >> {m.Groups[1].Value}");
-                var numbers = m.Groups[1].Value.Split(",");
-
-                if (numbers.Length != 2
-                    || !int.TryParse(numbers[0], out var number1)
-                    || !int.TryParse(numbers[1], out var number2))
-                {
-                    throw new ArgumentException($"Invalid numbers: {string.Join(", ", numbers)}");
-                }
-                
-                result += number1 * number2;
-            }
-
-            Console.WriteLine($"Part 1: {result}");
-            
+            Console.WriteLine($"Part 1: {CalculateMulFunctions(inputText)}");
+            Console.WriteLine($"Part 2: {CalculateMulFunctions(inputTextWithoutDonts)}");
             Console.ReadLine();
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
         }
+    }
+    
+    private static int CalculateMulFunctions(string inputText)
+    {
+        var pattern = @"mul\((\d{1,3},\d{1,3})\)";
+        var options = RegexOptions.Multiline;
+        var result = 0;
+        
+        foreach (Match m in Regex.Matches(inputText, pattern, options))
+        {
+            var numbers = m.Groups[1].Value.Split(",");
+
+            if (numbers.Length != 2
+                || !int.TryParse(numbers[0], out var number1)
+                || !int.TryParse(numbers[1], out var number2))
+            {
+                throw new ArgumentException($"Invalid numbers: {string.Join(", ", numbers)}");
+            }
+                
+            result += number1 * number2;
+        }
+        return result;
+    }
+    
+    private static string RemoveDonts(string inputText)
+    {
+        var pattern = @"don't\(\).*?do\(\)";
+        RegexOptions options = RegexOptions.Singleline | RegexOptions.Multiline;
+        return Regex.Replace(inputText, pattern, string.Empty, options);
     }
 }
